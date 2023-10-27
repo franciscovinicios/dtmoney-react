a867349bac5b2604a16c4bca7a44ca9b89065f2e
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-10-27 12:48:42 +0100

    visual structure: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionsButton,
+} from "./styles";
+import logoImg from "../../assets/logo.svg";
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
+}
