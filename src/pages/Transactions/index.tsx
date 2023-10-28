828392101ac38e7a19efade0eb564e45988c9b3b
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-10-28 13:46:32 +0100

    visual structure: Summary  component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary/inde";
 import { Container } from "./styles";
 
 export function Transactions() {
   return (
     <Container>
       <Header />
+      <Summary />
     </Container>
   );
 }
