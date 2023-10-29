daf50cb0870b67367dad6e22df914ca02a1a1847
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-10-29 01:29:05 +0100

    visual structure: transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/inde";
-import { Container } from "./styles";
+import {
+  Container,
+  PriceHighlight,
+  TransactionsContent,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <Container>
       <Header />
       <Summary />
+      <TransactionsContent>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width={"50%"}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/12/2022</td>
+            </tr>
+            <tr>
+              <td width={"50%"}>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/12/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContent>
     </Container>
   );
 }
