400eb9b606830b9ade63df7e8bfb5aea74127523
Francisco Vinicios <francisco.viniciosti@gmail.com>
2023-10-30 22:15:05 +0000

    connection with api: transactions context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary/inde";
@@ -8,28 +7,11 @@ import {
   TransactionsContent,
   TransactionsTable,
 } from "./styles";
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { TransactionsContext } from "../../context/TransactionsContext";
+import { useContext } from "react";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-
-    setTransactions(data);
-  }
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
   return (
     <Container>
       <Header />
